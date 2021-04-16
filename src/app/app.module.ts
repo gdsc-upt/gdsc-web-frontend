75ddf4170d2f7e7b77cea59614a3847d3c3c04ce
AnaM <ana.maghear@yahoo.com>
2021-04-16 21:11:45 +0300

    Added aboutus, articlespage, eventspage components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticlespageComponent } from './components/articlespage/articlespage.component';
+import { EventspageComponent } from './components/eventspage/eventspage.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticlespageComponent,
+    EventspageComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
