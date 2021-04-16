1a6e77b72bd79e2fc50c618e4602b2416ea00870
Emilia <emy_ioana_26@yahoo.com>
2021-04-16 22:00:55 +0300

    Setup routing for our Application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ContactComponent } from './components/contact/contact.component';
+import {MenuComponent} from './components/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'contactPath',  component: ContactComponent},
+  {path: 'menuPath' , component: MenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
