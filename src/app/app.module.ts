248f0efb7f29f942c299194a567cca75507d1f97
Marius <Mnv22081@gmail.com>
2021-04-16 21:07:39 +0300

    Added contact and homepage components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
