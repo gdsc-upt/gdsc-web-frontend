ace46ac4dd780b3c42eb266b95cd693d456f122c
Edi Cziszter <eduard.cziszter@student.upt.ro>
2021-05-11 00:01:56 +0300

    refactoring text-image

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {HomePageComponent} from './home-page.component';
-import { TextImageComponent } from './text-image/text-image.component';
+import { TextImageComponent } from './components/text-image/text-image.component';
 import { FaqsSectionComponent } from './components/faqs-section/faqs-section.component';
 import {MatExpansionModule} from '@angular/material/expansion';
 
