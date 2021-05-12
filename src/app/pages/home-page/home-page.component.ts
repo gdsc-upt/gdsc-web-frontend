483d8bd72681c2a7cd4f1433e48eca3dbb5356f5
RebornWinter <edy.lata2001@gmail.com>
2021-05-12 20:07:29 +0300

    started working on Contactform

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-homepage',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
 
+export class HomePageComponent implements OnInit {
+  name = new FormControl('');
   constructor() { }
 
   ngOnInit(): void {
--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {HomePageComponent} from './home-page.component';
+<<<<<<< Updated upstream
 import { TextImageComponent } from './components/text-image/text-image.component';
 import { FaqsSectionComponent } from './components/faqs-section/faqs-section.component';
 import {MatExpansionModule} from '@angular/material/expansion';
 
+=======
+import { TextImageComponent } from './text-image/text-image.component';
+import { ReactiveFormsModule } from '@angular/forms';
+>>>>>>> Stashed changes
 @NgModule({
   declarations: [
     HomePageComponent,
@@ -14,8 +19,14 @@ import {MatExpansionModule} from '@angular/material/expansion';
   ],
   imports: [
     CommonModule,
+<<<<<<< Updated upstream
     RouterModule.forChild([{path: '', component: HomePageComponent}]),
     MatExpansionModule,
+=======
+    RouterModule.forChild([{ path: '', component: HomePageComponent}]),
+    ReactiveFormsModule
+>>>>>>> Stashed changes
   ]
 })
 export class HomePageModule { }
+
