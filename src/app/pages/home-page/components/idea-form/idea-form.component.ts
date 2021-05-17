e1319672011f273b19130b1410d705839307e1f5
RebornWinter <edy.lata2001@gmail.com>
2021-05-18 00:28:39 +0300

    Update ideas form

--- src/app/pages/home-page/components/idea-form/idea-form.component.ts
+++ src/app/pages/home-page/components/idea-form/idea-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
@@ -6,21 +6,23 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   templateUrl: './idea-form.component.html',
   styleUrls: ['./idea-form.component.css'],
 })
-export class IdeaFormComponent implements OnInit{
+export class IdeaFormComponent implements OnInit {
   hide = true;
   ideasForm: FormGroup;
+
   ngOnInit(): void {
     this.ideasForm = new FormGroup({
-      name : new FormControl(''),
-      email : new FormControl('', [Validators.required, Validators.email]),
-      branch : new FormControl(''),
-      year : new FormControl(''),
-      idea : new FormControl('')
+      name: new FormControl(''),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      branch: new FormControl(''),
+      year: new FormControl(''),
+      idea: new FormControl('')
     });
   }
+
   getErrorMessage(): string {
     if (this.ideasForm.controls.email.hasError('required')) {
-      return 'You must enter a value';
+      return 'This field is required';
     }
 
     return this.ideasForm.controls.email.hasError('email') ? 'Not a valid email' : '';
