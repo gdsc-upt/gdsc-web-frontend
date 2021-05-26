37a51f45b2f14be9d82a5f19dae977bb8072ecb6
Marius <Mnv22081@gmail.com>
2021-05-26 21:33:18 +0300

    Added Article Preview

--- src/app/pages/articles-page/articles-page.module.ts
+++ src/app/pages/articles-page/articles-page.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ArticlesPageComponent } from './articles-page.component';
+import { ArticlePreviewComponent } from './article-preview/article-preview.component';
 
 const routes: Routes = [{path: '', component: ArticlesPageComponent}];
 
 @NgModule({
-  declarations: [ArticlesPageComponent],
+  declarations: [ArticlesPageComponent, ArticlePreviewComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class ArticlesPageModule {
---
+++ src/app/pages/articles-page/components/article-preview/article-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-preview',
+  templateUrl: './article-preview.component.html',
+  styleUrls: ['./article-preview.component.scss']
+})
+export class ArticlePreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
