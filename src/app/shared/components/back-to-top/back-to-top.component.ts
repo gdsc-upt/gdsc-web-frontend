514812f5b39b13f53446cccbd9d90f3b014f562a
RebornWinter <edy.lata2001@gmail.com>
2021-05-29 20:03:28 +0300

    Fixed pull request
    I changed the button size and margins on mobile and changed the function names to be more suggestive. Also I deleted the useless parts from ts(ngOnInit and constructors) as we don't use them yet. I made the cursor a hand on hover and made the bottom and right equal.

--- src/app/shared/components/back-to-top/back-to-top.component.ts
+++ src/app/shared/components/back-to-top/back-to-top.component.ts
@@ -1,26 +1,21 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-back-to-top',
   templateUrl: './back-to-top.component.html',
   styleUrls: ['./back-to-top.component.scss']
 })
-export class BackToTopComponent implements OnInit {
 
-  constructor() {
-  }
+export class BackToTopComponent {
 
-  ngOnInit(): void {
-  }
-
-  public topFunction(): void {
+  public goToTop(): void {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
 
   @HostListener('window:scroll', ['$event'])
 
-  public scrollFunction(): void {
+  public showOnScroll(): void {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       document.getElementById("myBtn").style.display = "block";
     } else {
