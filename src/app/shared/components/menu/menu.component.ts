0e02cb2fb6e426b7324a6e0647e0b060f03a7c20
Alexandru Lolea <alexandru.lolea@ligaac.ro>
2021-06-01 22:50:11 +0300

    Menu frumix v2

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -13,30 +13,35 @@ export class MenuComponent {
       type: 'button',
       link: '',
       id: 1,
+      logoName: 'home'
     },
     {
       name: 'Contact',
       type: 'button',
       link: '/contact',
       id: 2,
+      logoName: 'contact_support'
     },
     {
       name: 'Articles',
       type: 'button',
       link: '/articles',
       id: 3,
+      logoName: 'article'
     },
     {
       name: 'About Us',
       type: 'button',
       link: '/about-us',
       id: 4,
+      logoName: 'people'
     },
     {
       name: 'Events',
       type: 'button',
       link: '/events',
       id: 4,
+      logoName: 'event'
     },
   ];
 }
