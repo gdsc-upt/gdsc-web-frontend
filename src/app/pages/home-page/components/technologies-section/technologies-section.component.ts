28e849debe89c0b34e57db71a04ba965378e6cfd
Dan Percic <danpercic86@gmail.com>
2021-06-03 01:10:54 +0300

    turuk turuk

--- src/app/pages/home-page/components/technologies-section/technologies-section.component.ts
+++ src/app/pages/home-page/components/technologies-section/technologies-section.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ITechnology } from '@gdsc/models';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-technologies-section',
   templateUrl: './technologies-section.component.html',
-  styleUrls: ['./technologies-section.component.css']
+  styleUrls: ['./technologies-section.component.scss']
 })
 export class TechnologiesSectionComponent implements OnInit {
   offSet = 500;
+  _counter = -1;
+  audioSubject = new BehaviorSubject<HTMLAudioElement>(null);
+  counter$ = this.audioSubject.asObservable();
 
   technologies: ITechnology[] = [{
     id: '0',
@@ -16,36 +20,41 @@ export class TechnologiesSectionComponent implements OnInit {
     created: '',
     updated: ''
   },
-    {
-      id: '1',
-      image: 'assets/javascript.svg',
-      name: 'JavaScript',
-      created: '',
-      updated: ''
-    },
-    {
-      id: '2',
-      image: 'assets/python.jpg',
-      name: 'Python',
-      created: '',
-      updated: ''
-    },
-    {
-      id: '3',
-      image: 'assets/android.svg',
-      name: 'Android Development',
-      created: '',
-      updated: ''
-    },
+  {
+    id: '1',
+    image: 'assets/javascript.svg',
+    name: 'JavaScript',
+    created: '',
+    updated: ''
+  },
+  {
+    id: '2',
+    image: 'assets/python.jpg',
+    name: 'Python',
+    created: '',
+    updated: ''
+  },
+  {
+    id: '3',
+    image: 'assets/android.svg',
+    name: 'Android Development',
+    created: '',
+    updated: ''
+  }
   ];
   breakpoint: number;
 
   ngOnInit(): void {
     this.breakpoint = (innerWidth / this.offSet);
+
+    this.counter$.subscribe(async audio => {
+      this._counter = this._counter + 1;
+      if (this._counter > 1 && this._counter % 4 === 0) await audio.play();
+      else audio?.pause();
+    });
   }
 
   onResize(event): void {
     this.breakpoint = (event.target.innerWidth / this.offSet);
   }
-
 }
