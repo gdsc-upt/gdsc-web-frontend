f8ee46afb8ca28d1ad0f8d8d9efacfa8c0a1b55d
RebornWinter <edy.lata2001@gmail.com>
2021-06-03 17:47:42 +0300

    I got the Menu-Items from back-end

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,52 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IMenuItem } from '@gdsc/models';
+import { MenuItemsService } from '../../../services/menu-items.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {
-  menuItems: IMenuItem[] = [
-    {
-      name: 'Home',
-      type: 'button',
-      link: '',
-      id: '1',
-      created: '',
-      updated: ''
-    },
-    {
-      name: 'Contact',
-      type: 'button',
-      link: '/contact',
-      id: '2',
-      created: '',
-      updated: ''
-    },
-    {
-      name: 'Articles',
-      type: 'button',
-      link: '/articles',
-      id: '3',
-      created: '',
-      updated: ''
-    },
-    {
-      name: 'About Us',
-      type: 'button',
-      link: '/about-us',
-      id: '4',
-      created: '',
-      updated: ''
-    },
-    {
-      name: 'Events',
-      type: 'button',
-      link: '/events',
-      id: '4',
-      created: '',
-      updated: ''
-    },
-  ];
+export class MenuComponent implements OnInit {
+  menuItems: IMenuItem[];
+
+  constructor(private readonly menuItemsService: MenuItemsService) {
+  }
+
+  ngOnInit(): void {
+    this.menuItemsService.get().subscribe(menuItems => {
+      this.menuItems = menuItems;
+    });
+  }
 }
