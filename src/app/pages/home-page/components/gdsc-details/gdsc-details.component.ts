c9af9b5f1990e5fa041abeebf6c8a9aae967e68d
Iozsa Andrei <andrei.iozsa@student.upt.ro>
2021-06-05 23:58:08 +0300

    Styled the component and removed some unused assets

--- src/app/pages/home-page/components/gdsc-details/gdsc-details.component.ts
+++ src/app/pages/home-page/components/gdsc-details/gdsc-details.component.ts
@@ -1,23 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-gdsc-details',
   templateUrl: './gdsc-details.component.html',
   styleUrls: ['./gdsc-details.component.scss']
 })
-export class GdscDetailsComponent implements OnInit {
+export class GdscDetailsComponent {
+
+  title: string = 'Developer Student Clubs UPT';
+  text: string = 'Developer Student Clubs are university based community groups for ' +
+    'students interested in Google developer technologies.' +
+    'Students from all undergraduate or graduate programs with an interest ' +
+    'in growing as a developer are welcome!';
+  image: string = './assets/home.webp';
 
   constructor() {
   }
-
-  @Input() title: string;
-  @Input() text: string;
-  @Input() image: string;
-  @Input() fbImage: string;
-  @Input() gitImage: string;
-  @Input() webImage: string;
-
-  ngOnInit(): void {
-  }
-
 }
