dc68744fb7c5373f4c4cc1d664b09c9e512ad955
Dan Percic <danpercic86@gmail.com>
2021-06-21 11:16:20 +0300

    fix contact form image

--- src/app/shared/components/logo/logo.component.ts
+++ src/app/shared/components/logo/logo.component.ts
@@ -1,10 +1,9 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class LogoComponent {
   @Input() small = false;
