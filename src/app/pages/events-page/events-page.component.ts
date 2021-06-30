0a13a3b05a2d8ffb52f0c6646a2e1a08e05219a9
RebornWinter <edy.lata2001@gmail.com>
2021-06-30 17:03:53 +0300

    New Event Page

--- src/app/pages/events-page/events-page.component.ts
+++ src/app/pages/events-page/events-page.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 
+export interface EventTable {
+  name: string;
+  date: string;
+  status: string;
+}
+
+const EVENT_DATA: EventTable[] = [
+  {name: 'Angular Workshop', date: '29/05/2021 19:00', status: 'Event in progress'},
+  {name: '.NET Workshop', date: '29/05/2021 19:00', status: 'Sign-up Closed'},
+  {name: 'C# Workshop', date: '29/05/2021 19:00', status: 'Sign-up Open'},
+  {name: 'Angular Workshop', date: '29/05/2021 19:00', status: 'Sign-up Open'},
+  {name: '.NET Workshop', date: '29/05/2021 19:00', status: 'Sign-up Closed'},
+  {name: '.NET Workshop', date: '29/05/2021 19:00', status: 'Sign-up Closed'},
+  {name: '.NET Workshop', date: '29/05/2021 19:00', status: 'Sign-up Closed'},
+  {name: '.NET Workshop', date: '29/05/2021 19:00', status: 'Sign-up Closed'}
+];
+
 @Component({
   templateUrl: './events-page.component.html',
   styleUrls: ['./events-page.component.scss']
 })
 export class EventsPageComponent {
+  displayedColumns: string[] = ['name', 'date', 'status'];
+  dataSource = EVENT_DATA;
 }
--- src/app/pages/events-page/events-page.module.ts
+++ src/app/pages/events-page/events-page.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { EventsPageComponent } from './events-page.component';
 import { SharedModule } from '../../shared/shared.module';
+import { MatTableModule } from '@angular/material/table';
 
 const routes: Routes = [{ path: '', component: EventsPageComponent }];
 
 @NgModule({
   declarations: [EventsPageComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), SharedModule]
+  imports: [CommonModule, RouterModule.forChild(routes), SharedModule, MatTableModule]
 })
 export class EventsPageModule {
 }
