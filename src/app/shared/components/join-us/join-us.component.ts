1aa9797e84cd8ab4d383f3d45568fcaf5ec44818
Leonardo <leotoma4@gmail.com>
2021-10-10 22:14:30 +0300

    create join component and button

--- src/app/shared/components/join-us/join-us.component.ts
+++ src/app/shared/components/join-us/join-us.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { EPositions } from '@gdsc/types';
 
 @Component({
+  selector:'gdsc-join-us',
   templateUrl: './join-us.component.html',
   styleUrls: ['./join-us.component.scss']
 })
