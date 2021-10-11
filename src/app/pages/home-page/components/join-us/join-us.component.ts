82b549a5e800c66777352fd27553cfc568e46aa7
Leonardo <leotoma4@gmail.com>
2021-10-11 11:58:15 +0300

    fixed and changed button color

--- src/app/pages/home-page/components/join-us/join-us.component.ts
+++ src/app/pages/home-page/components/join-us/join-us.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { EPositions } from '@gdsc/types';
 
 @Component({
-  selector:'gdsc-join-us',
+  selector: 'gdsc-join-us',
   templateUrl: './join-us.component.html',
   styleUrls: ['./join-us.component.scss']
 })
-
 export class JoinUsComponent {
-  readonly positions = EPositions;}
+  readonly positions = EPositions;
+}
