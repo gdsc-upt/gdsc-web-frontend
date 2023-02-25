5c5eaab8cf886b2e3e125980dd4d78ea5ab4307f
Daniel <turutadaniel20@gmail.com>
2023-02-25 18:27:31 +0200

    event page

--- src/app/pages/events-page/events-page.module.ts
+++ src/app/pages/events-page/events-page.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { EventsPageComponent } from './events-page.component';
 import { SharedModule } from '../../shared/shared.module';
+import { CardComponent } from './card/card.component';
+import { MatCardModule } from '@angular/material/card';
 
 const routes: Routes = [{ path: '', component: EventsPageComponent }];
 
 @NgModule({
-  declarations: [EventsPageComponent],
+  declarations: [EventsPageComponent,CardComponent],
   exports: [
     EventsPageComponent
   ],
-  imports: [CommonModule, RouterModule.forChild(routes), SharedModule]
+  imports: [CommonModule, RouterModule.forChild(routes), SharedModule, MatCardModule]
 })
 export class EventsPageModule {
 }
